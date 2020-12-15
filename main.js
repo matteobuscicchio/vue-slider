f3cba823a34e1c1bf87318cd4793ecba53ea8067
matteo <matteoezio@me.com>
2020-12-15 16:40:02 +0100

    Carosello creato e html parzialmente  adattato

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 // immagini per l'array
-// "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg"
-// "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg"
-// "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg"
-// "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
+// "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+// "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+// "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+// "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg",
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        index: 0,
+        imgs: [
+            "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+            "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+            "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+            "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg",
+        ]
+    },
+    methods: {}
+});
