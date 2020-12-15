cb714744cdcc38194639435bd223b124e6ad61db
matteo <matteoezio@me.com>
2020-12-15 18:06:25 +0100

    corretta la linea 36 del file js
    l'uguale presente nell'if impediva la riproduzione della foto in posizione 0 nell'array

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+// Istruzioni:
+// Ricreare lo slider di immagini ma questa volta con Vue
+// Bonus ufficiale:
+// Facciamo in modo che il nostro slider scorra automaticamente in avanti,
+// e ogni 3 secondi visualizzi un immagine differente.
+// Tip: possiamo usare una lifecycle Hook per inserire un nostro script quando l'istanza Vue é stata caricata.
+// Possibili extra Bonus:
+// cliccando sui pallini visualizzeremo l'immagine corrispondente
+// cliccando sulle frecce della tastiera sinista e destra scorriamo tra le immagini
+// I bonus metteteli in una cartella "bonus".
+
 let app = new Vue({
+
     el: '#app',
 
     data: {
@@ -18,9 +30,10 @@ let app = new Vue({
                 this.index = 0;
             };
         },
+        
         prev(){
             this.index--;
-            if (this.index <= 0) {
+            if (this.index < 0) {
                 this.index = this.imgs.length -1;
             };
         }
