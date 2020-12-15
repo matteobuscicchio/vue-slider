8115cc7a901297ebbca977d227b97d8107adc1bf
matteo <matteoezio@me.com>
2020-12-15 17:18:36 +0100

    Rese funzionanti le frecce di navigazione

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-// immagini per l'array
-// "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
-// "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
-// "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
-// "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg",
-
 let app = new Vue({
     el: '#app',
+
     data: {
         index: 0,
         imgs: [
@@ -15,5 +10,19 @@ let app = new Vue({
             "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg",
         ]
     },
-    methods: {}
+
+    methods: {
+        next(){
+            this.index++;
+            if (this.index >= this.imgs.length) {
+                this.index = 0;
+            };
+        },
+        prev(){
+            this.index--;
+            if (this.index <= 0) {
+                this.index = this.imgs.length -1;
+            };
+        }
+    }
 });
